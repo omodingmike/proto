4cda400a0e2725621d51e9709572296b80a97ba3
MIKE <mike@MacBook-Air.local>
2024-01-19 07:47:45 +0300

    now

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -14,9 +14,6 @@ export default function MyCarousel() {
                              alt="image description" loading={"lazy"}/>
                         <div className="absolute inset-0 bg-black opacity-40"></div>
                         <div className="absolute inset-0 flex items-center justify-center">
-                            {/*<div>*/}
-                            {/*    <h1 className=" text-white text-2xl sm:text-4xl font-bold dark:text-white text-center">TRAVEL TO EXPLORE</h1>*/}
-                            {/*</div>*/}
                             <div className="text-center">
                                 <div role="status">
                                     <svg aria-hidden="true" className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
--- tailwind.config.js
+++ tailwind.config.js
@@ -61,6 +61,6 @@ export default {
     }
     ,
     plugins: [
-        require('flowbite/plugin')
+        require("flowbite/plugin")
     ]
 }
